3c0441093e75b8fc70182221df6866b904700420
kahlilwassell <kahlilwassell@gmail.com>
2024-11-24 18:33:21 -0600

    update script to automate file movement and table fix

--- batch_layer/setup_cta_ridership_data.sh
+++ batch_layer/setup_cta_ridership_data.sh
@@ -45,5 +45,10 @@ else
     echo "Hive table for ridership created and data loaded successfully."
 fi
 
+# move files and repair table for ridership
+hadoop fs -put $HOME/kjwassell/MPCS53014FinalProject/raw_data/CTA_L_Ridership_Daily_Totals.csv \
+wasbs://hive-mpcs53014-2024-11-10t16-50-58-559z@hivempcs53014hdistorage.blob.core.windows.net/kjwassell/cta_data/stations/
+beeline -u "jdbc:hive2://10.0.0.50:10001/;transportMode=http" -e "MSCK REPAIR TABLE kjwassell_cta_ridership_csv;"
+
 # Success message
 echo "Ridership data setup completed successfully."
